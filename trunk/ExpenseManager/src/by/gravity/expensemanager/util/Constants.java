6721fe2c8384190a8c1f0e443bd40a9668374a46
ilya.shknaj <ilya.shknaj@77f0ca8b-8ad4-adc7-5ef0-868d89e670e5>
2014-02-18 13:22:30 +0000

    show timePickerDialog added constants

---
+++ trunk/ExpenseManager/src/by/gravity/expensemanager/util/Constants.java
@@ -0,0 +1,50 @@
+package by.gravity.expensemanager.util;
+
+public class Constants {
+	public static final String SPACE_STRING = " ";
+	
+	public static final String EMPTY_STRING = "";
+
+	public static final String ZERO_STRING = "0";
+
+	public static final String COLON_STRING = ":";
+
+	public static final String DOT_STRING = ".";
+
+	public static final String COMMA_STRING = ",";
+
+	public static final char SPACE_CHAR = ' ';
+
+	public static final char COMMA_CHAR = ',';
+
+	public static final String CATEGORY_SPLITTER = ", ";
+
+	public static final String ONE_STRING = "1";
+
+	public static final String TWO_STRING = "2";
+
+	public static final String THREE_STRING = "3";
+
+	public static final String FOUR_STRING = "4";
+
+	public static final String FIVE_STRING = "5";
+
+	public static final String SIX_STRING = "6";
+
+	public static final String SEVEN_STRING = "7";
+
+	public static final String EIGHT_STRING = "8";
+
+	public static final String NINE_STRING = "9";
+
+	public static final String DOUBLE_ZERO_STRIG = "00";
+
+	public static final char PLUS_CHAR = '+';
+
+	public static final char MINUS_CHAR = '-';
+
+	public static final char MULTIPLE_CHAR = '*';
+
+	public static final char DEVIDE_CHAR = '/';
+
+}
