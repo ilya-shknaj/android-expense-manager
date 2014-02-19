c4c07776b4389b198fcec7f0a2c53154226227ab
ilya.shknaj <ilya.shknaj@77f0ca8b-8ad4-adc7-5ef0-868d89e670e5>
2014-02-19 13:43:55 +0000

    added SQLDataManager

---
+++ ExpenseManager/src/by/gravity/expensemanager/data/helper/SQLConstants.java
@@ -0,0 +1,23 @@
+package by.gravity.expensemanager.data.helper;
+
+public class SQLConstants {
+
+	public static final String TABLE_EXPENSE = "Expense";
+	public static final String TABLE_CATEGORY = "Category";
+	public static final String TABLE_EXPENSE_CATEGORY = "Expsense_Category";
+	public static final String TABLE_CURRENCY = "Currency";
+	public static final String TABLE_PAYMENT_METHODS = "PaymentMethods";
+
+	public static final String FIELD_ID = "id";
+	public static final String FIELD_AMOUNT = "amount";
+	public static final String FIELD_CURRENCY = "currency";
+	public static final String FIELD_DATE = "date";
+	public static final String FIELD_NOTE = "note";
+	public static final String FIELD_PAYMENT_METHOD = "paymentMethod";
+	public static final String FIELD_NAME = "name";
+	public static final String FIELD_EXPENSE_ID = "expenseId";
+	public static final String FIELD_CATEGORY_ID = "categoryId";
+	public static final String FIELD_CODE = "code";
+	public static final String FIELD_SYMBOL = "symbol";
+	public static final String FIELD_BALANCE = "balance";
+}
