7f2fc42e9708c5838641d4e9ced566d44aed816d
ilya.shknaj <ilya.shknaj@77f0ca8b-8ad4-adc7-5ef0-868d89e670e5>
2014-03-25 13:35:10 +0000

    don't show setting and drawer layout whene not need

--- ExpenseManager/src/by/gravity/expensemanager/activity/DrawerActivity.java
+++ ExpenseManager/src/by/gravity/expensemanager/activity/DrawerActivity.java
@@ -36,14 +36,23 @@ public class DrawerActivity extends SherlockFragmentActivity {
 
 	@Override
 	protected void onCreate(Bundle arg0) {
+
 		super.onCreate(arg0);
 		setContentView(R.layout.a_main);
 		if (getIntent().getAction().equals(Intent.ACTION_MAIN)) {
 			initDrawer();
 		}
+
+		if (getCallingActivity() != null) {
+			if (mDrawerLayout == null) {
+				mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+			}
+			mDrawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED);
+		}
 	}
 
 	private void initDrawer() {
+
 		mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
 		listView = (ListView) findViewById(R.id.leftDrawer);
 
@@ -70,9 +79,9 @@ public class DrawerActivity extends SherlockFragmentActivity {
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
+
 		/*
-		 * The action bar home/up action should open or close the drawer.
-		 * mDrawerToggle will take care of this.
+		 * The action bar home/up action should open or close the drawer. mDrawerToggle will take care of this.
 		 */
 		if (mDrawerToggle.onOptionsItemSelected(item)) {
 			return true;
@@ -81,6 +90,7 @@ public class DrawerActivity extends SherlockFragmentActivity {
 	}
 
 	private void setActiveMenuItem(int position) {
+
 		int size = listView.getAdapter().getCount();
 		for (int i = 0; i < size; i++) {
 			if (position == i) {
@@ -94,18 +104,20 @@ public class DrawerActivity extends SherlockFragmentActivity {
 
 	@Override
 	public void onConfigurationChanged(Configuration newConfig) {
+
 		super.onConfigurationChanged(newConfig);
 		mDrawerToggle.onConfigurationChanged(newConfig);
 	}
 
 	/**
-	 * This list item click listener implements very simple view switching by
-	 * changing the primary content text. The drawer is closed when a selection
+	 * This list item click listener implements very simple view switching by changing the primary content text. The drawer is closed when a selection
 	 * is made.
 	 */
 	private class DrawerItemClickListener implements ListView.OnItemClickListener {
+
 		@Override
 		public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
 			MenuModel selectedItem = DRAWER_MENU.get(position);
 			mActionBar.setTitle(selectedItem.getText());
 			mDrawerLayout.closeDrawer(listView);
@@ -121,38 +133,41 @@ public class DrawerActivity extends SherlockFragmentActivity {
 	}
 
 	/**
-	 * A drawer listener can be used to respond to drawer events such as
-	 * becoming fully opened or closed. You should always prefer to perform
-	 * expensive operations such as drastic relayout when no animation is
-	 * currently in progress, either before or after the drawer animates.
+	 * A drawer listener can be used to respond to drawer events such as becoming fully opened or closed. You should always prefer to perform
+	 * expensive operations such as drastic relayout when no animation is currently in progress, either before or after the drawer animates.
 	 * 
-	 * When using ActionBarDrawerToggle, all DrawerLayout listener methods
-	 * should be forwarded if the ActionBarDrawerToggle is not used as the
+	 * When using ActionBarDrawerToggle, all DrawerLayout listener methods should be forwarded if the ActionBarDrawerToggle is not used as the
 	 * DrawerLayout listener directly.
 	 */
 	private class DemoDrawerListener implements DrawerLayout.DrawerListener {
+
 		@Override
 		public void onDrawerOpened(View drawerView) {
+
 			mDrawerToggle.onDrawerOpened(drawerView);
 		}
 
 		@Override
 		public void onDrawerClosed(View drawerView) {
+
 			mDrawerToggle.onDrawerClosed(drawerView);
 		}
 
 		@Override
 		public void onDrawerSlide(View drawerView, float slideOffset) {
+
 			mDrawerToggle.onDrawerSlide(drawerView, slideOffset);
 		}
 
 		@Override
 		public void onDrawerStateChanged(int newState) {
+
 			mDrawerToggle.onDrawerStateChanged(newState);
 		}
 	}
 
 	private static List<MenuModel> getDrawerMenu() {
+
 		List<MenuModel> drawerMenu = new ArrayList<MenuModel>();
 		Context context = ContextHolder.getContext();
 		drawerMenu.add(new MenuModel(0, context.getString(R.string.main), true));
