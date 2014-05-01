b4fa127e8143a834957b879b860816e6e3ea4483
ilya.shknaj <ilya.shknaj@77f0ca8b-8ad4-adc7-5ef0-868d89e670e5>
2014-05-01 17:27:05 +0000

    fix end period date

--- ExpenseManager/src/by/gravity/expensemanager/fragments/loaders/GroupedByDateCursorLoader.java
+++ ExpenseManager/src/by/gravity/expensemanager/fragments/loaders/GroupedByDateCursorLoader.java
@@ -2,7 +2,6 @@ package by.gravity.expensemanager.fragments.loaders;
 
 import android.content.Context;
 import android.database.Cursor;
-import android.net.Uri;
 import android.support.v4.content.CursorLoader;
 import by.gravity.expensemanager.data.SQLDataManager;
 
@@ -10,10 +9,6 @@ public class GroupedByDateCursorLoader extends CursorLoader {
 
 	public GroupedByDateCursorLoader(Context context) {
 		super(context);
-	}
-
-	public GroupedByDateCursorLoader(Context context, Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
-		super(context, uri, projection, selection, selectionArgs, sortOrder);
 	}
 
 	@Override
